1c9aaebc433df74c21670655fc74b0ae872235e2
tprudhomme <tprudhomme@tranquil.it>
2025-11-25 16:21:00 +0100

    [NEW] Property Attributes - Create frame

---
+++ packages/OpenRSATCore/PropertyPages/ufrmpropertyattributes.pas
@@ -0,0 +1,207 @@
+unit ufrmpropertyattributes;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes,
+  SysUtils,
+  Forms,
+  Controls,
+  StdCtrls,
+  Buttons, ExtCtrls, ActnList,
+  tis.ui.grid.core,
+  mormot.core.base,
+  mormot.core.log, mormot.core.variants, mormot.net.ldap,
+  uproperty,
+  upropertyframe, VirtualTrees, Graphics;
+
+type
+
+  { TFrmPropertyAttributes }
+
+  TFrmPropertyAttributes = class(TPropertyFrame)
+    Action_Filter: TAction;
+    Action_Modify: TAction;
+    ActionList1: TActionList;
+    BitBtn_Modify: TBitBtn;
+    BitBtn_Filter: TBitBtn;
+    Label_Attributes: TLabel;
+    List_Attributes: TTisGrid;
+    Panel1: TPanel;
+    Timer_SearchInGrid: TTimer;
+    procedure Action_ModifyExecute(Sender: TObject);
+    procedure Action_ModifyUpdate(Sender: TObject);
+    procedure List_AttributesDblClick(Sender: TObject);
+    procedure List_AttributesDrawText(Sender: TBaseVirtualTree;
+      TargetCanvas: TCanvas; Node: PVirtualNode; Column: TColumnIndex;
+      const CellText: String; const CellRect: TRect; var DefaultDraw: Boolean);
+    procedure List_AttributesGetText(aSender: TBaseVirtualTree;
+      aNode: PVirtualNode; const aCell: TDocVariantData; aColumn: TColumnIndex;
+      aTextType: TVSTTextType; var aText: string);
+    procedure List_AttributesKeyPress(Sender: TObject; var Key: char);
+    procedure Timer_SearchInGridTimer(Sender: TObject);
+  private
+    fLog: TSynLog;
+    fProperty: TProperty;
+    fSearchWord: RawUtf8;
+  public
+    constructor Create(TheOwner: TComponent); override;
+    procedure Update(Props: TProperty); override;
+  end;
+
+implementation
+uses
+  mormot.core.text,
+  ucommon,
+  uhelpers,
+  uvisattributeeditor;
+
+{$R *.lfm}
+
+{ TFrmPropertyAttributes }
+
+procedure TFrmPropertyAttributes.Timer_SearchInGridTimer(Sender: TObject);
+begin
+  Timer_SearchInGrid.Enabled := False;
+end;
+
+procedure TFrmPropertyAttributes.List_AttributesKeyPress(Sender: TObject;
+  var Key: char);
+begin
+  SearchInGrid(Timer_SearchInGrid, List_Attributes, fSearchWord, Key);
+end;
+
+procedure TFrmPropertyAttributes.List_AttributesDblClick(Sender: TObject);
+begin
+  Action_Modify.Execute;
+end;
+
+procedure TFrmPropertyAttributes.Action_ModifyUpdate(Sender: TObject);
+begin
+  Action_Modify.Enabled := Assigned(List_Attributes.FocusedRow);
+end;
+
+procedure TFrmPropertyAttributes.Action_ModifyExecute(Sender: TObject);
+var
+  vis: TVisAttributeEditor;
+  Attribute: TLdapAttribute;
+  i: Integer;
+begin
+  Attribute := fProperty.Get(List_Attributes.FocusedRow^.S['attribute']);
+
+  vis := TVisAttributeEditor.Create(self, fProperty.Core, Attribute, List_Attributes.FocusedRow^.S['attribute']);
+  try
+    if (vis.ShowModal <> mrOK) then
+      Exit;
+
+    if Vis.Attr.Count <= 0 then
+    begin
+      fProperty.Add(Vis.Attr.AttributeName, '');
+      Exit;
+    end;
+    fProperty.Add(Vis.Attr.AttributeName, Vis.Attr.GetReadable(0));
+    for i := 1 to Vis.Attr.Count - 1 do
+      fProperty.Add(Vis.Attr.AttributeName, Vis.Attr.GetReadable(i), aoAlways);
+  finally
+    FreeAndNil(vis);
+  end;
+end;
+
+procedure TFrmPropertyAttributes.List_AttributesDrawText(
+  Sender: TBaseVirtualTree; TargetCanvas: TCanvas; Node: PVirtualNode;
+  Column: TColumnIndex; const CellText: String; const CellRect: TRect;
+  var DefaultDraw: Boolean);
+var
+  NodeData: PDocVariantData;
+  AttributeName: RawUtf8;
+begin
+  if Column <> 1 then
+    Exit;
+
+  NodeData := List_Attributes.GetNodeAsPDocVariantData(Node);
+  if not Assigned(NodeData) then
+    Exit;
+  AttributeName := NodeData^.U['attribute'];
+
+
+  if not fProperty.IsModified(AttributeName) then // check if diff exists
+    Exit;
+
+  if fProperty.GetReadable(AttributeName) = '' then
+    TargetCanvas.Font.Style := [TFontStyle.fsItalic, TFontStyle.fsStrikeOut]
+  else
+    TargetCanvas.Font.Style := [TFontStyle.fsItalic, TFontStyle.fsBold];
+end;
+
+procedure TFrmPropertyAttributes.List_AttributesGetText(
+  aSender: TBaseVirtualTree; aNode: PVirtualNode; const aCell: TDocVariantData;
+  aColumn: TColumnIndex; aTextType: TVSTTextType; var aText: string);
+var
+  NodeData: PDocVariantData;
+  AttributeName: RawUtf8;
+  AttributeValues: TRawUtf8DynArray;
+  len: SizeInt;
+  GridColumn: TTisGridColumn;
+begin
+  if aColumn <> 1 then
+    Exit;
+
+  NodeData := List_Attributes.GetNodeAsPDocVariantData(aNode);
+  if not Assigned(NodeData) then
+    Exit;
+  AttributeName := NodeData^.U['attribute'];
+
+  AttributeValues := fProperty.GetAllReadable(AttributeName);
+
+  aText := String.Join('; ', TStringArray(AttributeValues));
+  GridColumn := List_Attributes.FindColumnByIndex(aColumn);
+  if GridColumn.Width <= List_Attributes.Canvas.TextWidth(aText) then
+  begin
+    len := Length(AttributeValues);
+    aText := FormatUtf8('%; range=0-%', [AttributeValues[0], Len]);
+  end;
+end;
+
+constructor TFrmPropertyAttributes.Create(TheOwner: TComponent);
+begin
+  inherited Create(TheOwner);
+
+  fLog := TSynLog.Add;
+  if Assigned(fLog) then
+    fLog.Log(sllTrace, 'Create', Self);
+
+  Caption := 'Attributes';
+end;
+
+procedure TFrmPropertyAttributes.Update(Props: TProperty);
+var
+  attribute: TLdapAttribute;
+  Row: TDocVariantData;
+begin
+  if Assigned(fLog) then
+    fLog.Log(sllTrace, 'Update', Self);
+
+  fProperty := Props;
+
+  List_Attributes.BeginUpdate;
+  try
+    Row.init();
+    for attribute in fProperty.Attributes.Items do
+    begin
+      if not Assigned(attribute) then
+        continue;
+      Row.AddOrUpdateValue('attribute', attribute.AttributeName);
+      Row.AddOrUpdateValue('value', attribute.GetVariant());
+      List_Attributes.Data.AddItem(Row);
+      Row.Clear;
+    end;
+  finally
+    List_Attributes.EndUpdate;
+    List_Attributes.LoadData();
+  end;
+end;
+
+end.
+
