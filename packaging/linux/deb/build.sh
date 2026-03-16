a3c1676066b65a5d9d523c7432fe316aa4c748eb
tprudhomme <tprudhomme@tranquil.it>
2026-03-16 14:53:02 +0100

    [UPD] DEB - Ensure files exists

--- packaging/linux/deb/build.sh
+++ packaging/linux/deb/build.sh
@@ -3,11 +3,22 @@
 # Usage:
 # ./build.sh <VERSION:0.4.226> <ARCH:x64,arm64,arm32,x86> <ICON_PATH:./icon.png>
 
+USAGE="./build.sh <VERSION:0.4.226> <ARCH:x64,arm64,arm32,x86> <ICON_PATH:./icon.png>"
+
+set -ex
+
 VERSION=$1
 ARCH=$2
 ICON_PATH=$3
-URL="https://github.com/tranquilit/openrsat/releases/download/v"$1
+URL="https://github.com/tranquilit/openrsat/releases/download/v"$VERSION
 BUILDDIR=$PWD/builddir
+
+if [ "$#" -ne 3 ]; then
+  echo "Invalid number of arguments."
+  echo "Usage:"
+  echo "	$USAGE"
+  exit
+fi
 
 echo "Verify architecture..."
 
@@ -45,13 +56,14 @@ echo "Change version in control file."
 sed "s/VERSION/$VERSION/" -i $BUILDDIR/DEBIAN/control
 
 echo "Download binary from architecture."
-wget -O ./OpenRSAT $URL/OpenRSAT-linux-$ARCH
+wget -O $BUILDDIR/opt/openrsat/OpenRSAT $URL/OpenRSAT-linux-$ARCH
 
-echo "Move downloaded binary."
-mv ./OpenRSAT $BUILDDIR/opt/openrsat/
 echo "Copy icon."
+[ -f "$ICON_PATH" ] || (echo "$ICON_PATH does not exists." && exit)
 cp $ICON_PATH $BUILDDIR/opt/openrsat/OpenRSAT.png
+
 echo "Copy desktop file."
+[ -f "./OpenRSAT.desktop" ] || (echo "./OpenRSAT.desktop does not exists." && exit)
 cp ./OpenRSAT.desktop $BUILDDIR/usr/share/applications/
 
 echo "Set permissions to binary, icon and desktop files."
@@ -60,7 +72,7 @@ chmod 755 $BUILDDIR/opt/openrsat/OpenRSAT.png
 chmod 755 $BUILDDIR/usr/share/applications/OpenRSAT.desktop
 
 echo "Build deb file."
-dpkg-deb --build $BUILDDIR OpenRSAT-${DEBARCH}-${VERSION}.deb
+dpkg-deb --build $BUILDDIR OpenRSAT-${VERSION}-${DEBARCH}.deb
 
 echo "Remove build folder."
 rm -rf $BUILDDIR
