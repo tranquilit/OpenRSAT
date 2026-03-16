fc450f5f80fb25c93208b577c86e42914cca6800
tprudhomme <tprudhomme@tranquil.it>
2026-03-16 14:06:09 +0100

    [FIX] RPM - Build arch

--- packaging/linux/rpm/build.sh
+++ packaging/linux/rpm/build.sh
@@ -13,33 +13,34 @@ URL="https://github.com/tranquilit/openrsat/releases/download/v"$VERSION
 echo "Verify architecture..."
 
 if [ "$ARCH" = "x64" ]; then
-  DEBARCH=amd64
+  RPMARCH=x86_64
 elif [ "$ARCH" = "arm64" ]; then
-  DEBARCH=arm64
+  RPMARCH=arm64
 elif [ "$ARCH" = "arm32" ]; then
-  DEBARCH=armhf
-elif [ "$ARCH" = "x86" ]; then
-  DEBARCH=i386
+  RPMARCH=arm32
+elif [ "$ARCH" = "i386" ]; then
+  RPMARCH=i386
 else
   echo "Invalid architecture. Exit."
   exit
 fi
 
 echo "Setting up environment"
-mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES}
+mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS}
 
 echo "Download from $URL"
 wget -O ~/rpmbuild/SOURCES/OpenRSAT $URL/OpenRSAT-linux-$ARCH
 cp $ICON_PATH ~/rpmbuild/SOURCES/OpenRSAT.png
 cp ./OpenRSAT.desktop ~/rpmbuild/SOURCES/OpenRSAT.desktop
+cp ./OpenRSAT.spec ~/rpmbuild/SPECS/OpenRSAT.spec
 
 echo "Build rpm"
-rpmbuild -bb --buildroot $BUILDDIR --define "_version $VERSION" --clean ./OpenRSAT.spec
+rpmbuild -bb --buildroot $BUILDDIR --define "_version $VERSION" --define "_rpmarch $RPMARCH" --clean ~/rpmbuild/SPECS/OpenRSAT.spec
 
 echo "Retrieve rpm"
 mv ~/rpmbuild/RPMS/*/*.rpm .
 
 echo "Remove builddir"
-rm -rf ~/rpmbuild
+rm -rf ~/rpmbuild $BUILDDIR
 
 echo "Success"
