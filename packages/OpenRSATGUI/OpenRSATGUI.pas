4159a4e8b1c5724ad0568e39efb1baed4905c3ba
mmarsac <mmarsac@tranquil.it>
2026-04-20 17:29:39 +0200

    [FIX] OpenRSAT - Add new object creator to compilation

--- packages/OpenRSATGUI/OpenRSATGUI.pas
+++ packages/OpenRSATGUI/OpenRSATGUI.pas
@@ -32,7 +32,7 @@ uses
   uvismodifygplink, uvisprofilemanager, uviseditaduccolumns, 
   uvisprofileconfiguration, uvisrootdseinfos, uvisshowrelationship, utheme, 
   ufrmpropertyntauthcertificates, ufrmpropertyunixattributes, 
-  LazarusPackageIntf;
+  ufrmnewinetorgperson, LazarusPackageIntf;
 
 implementation
 
