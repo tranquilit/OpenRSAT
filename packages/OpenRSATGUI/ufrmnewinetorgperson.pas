87ab13a757c2913d79dcd6b63e52290be3684c76
mmarsac <mmarsac@tranquil.it>
2026-04-21 10:54:01 +0200

    [NEW] NewObject - Add last page of InetOrgPerson object builder

--- packages/OpenRSATGUI/ufrmnewinetorgperson.pas
+++ packages/OpenRSATGUI/ufrmnewinetorgperson.pas
@@ -33,6 +33,7 @@ type
     Edit_LastName: TEdit;
     Edit_Initials: TEdit;
     Edit_FirstName: TEdit;
+    Label_Validation: TLabel;
     Label_Password: TLabel;
     Label_Confirm: TLabel;
     Label_PreWindows: TLabel;
@@ -41,25 +42,27 @@ type
     Label_LastName: TLabel;
     Label_Initials: TLabel;
     Label_FirstName: TLabel;
+    Memo_Properties: TMemo;
     Panel1: TPanel;
     Panel2: TPanel;
     Panel3: TPanel;
     Panel4: TPanel;
     Panel5: TPanel;
+    Panel6: TPanel;
     procedure Action_BackExecute(Sender: TObject);
     procedure Action_BackUpdate(Sender: TObject);
     procedure Action_NextExecute(Sender: TObject);
     procedure Action_NextUpdate(Sender: TObject);
     procedure CheckBox_CannotChangeClick(Sender: TObject);
     procedure CheckBox_NeverExpiresClick(Sender: TObject);
-    procedure CheckBox_NextLogonClick(Sender: TObject);
+    procedure CheckBox_NextLogonChange(Sender: TObject);
     procedure Edit_FirstNameChange(Sender: TObject);
     procedure Edit_InitialsChange(Sender: TObject);
     procedure Edit_LastNameChange(Sender: TObject);
     procedure Edit_UserLogonNameChange(Sender: TObject); 
   private
     PageID: Integer;
-    procedure Load;
+    procedure BuildRecapProperties;
   public
     constructor Create(TheOwner: TComponent); override;
   end;
@@ -79,6 +82,21 @@ uses
 
 {$R *.lfm}
 
+procedure TFrmNewInetOrgPerson.BuildRecapProperties;
+begin
+  Memo_Properties.Lines.Clear;
+  Memo_Properties.Lines.Add(FormatUtf8('Full name: %%', [Edit_FullName.Text, LineEnding]));
+  Memo_Properties.Lines.Add(FormatUtf8('User logon name: %%', [Edit_UserLogonName.Text, LineEnding]));
+  if CheckBox_NextLogon.Checked then
+    Memo_Properties.Lines.Add(CheckBox_NextLogon.Caption);
+  if CheckBox_CannotChange.Checked then
+    Memo_Properties.Lines.Add(CheckBox_CannotChange.Caption);
+  if CheckBox_NeverExpires.Checked then
+    Memo_Properties.Lines.Add(CheckBox_NeverExpires.Caption);
+  if CheckBox_AccountDisabled.Checked then
+    Memo_Properties.Lines.Add(CheckBox_AccountDisabled.Caption);
+end;
+
 procedure TFrmNewInetOrgPerson.Edit_FirstNameChange(Sender: TObject);
 var
   FullName: String;
@@ -92,6 +110,9 @@ begin
 end;
 
 procedure TFrmNewInetOrgPerson.Action_NextExecute(Sender: TObject);
+var
+  NewObject: TVisNewObject;
+  Att: TLdapAttributeList;
 begin
   if PageID = 0 then
   begin
@@ -110,8 +131,22 @@ begin
       Exit
     end;
     PageID += 1;
+    BuildRecapProperties;
     Panel4.Visible := False;
     Panel5.Visible := False;
+    Panel6.Visible := True; 
+  end
+  else if PageID = 2 then
+  begin
+    NewObject := (owner as TVisNewObject);
+    Att := TLdapAttributeList.Create();
+    try
+      Att.Add('objectClass', 'top').Add('person');
+      //'organizationalPerson', 'user', 'inetOrgPerson']
+      NewObject.Ldap.Add('CN=' + Edit_FullName.Text + ',' + NewObject.ObjectOU, Att)
+    finally
+      FreeAndNil(Att);
+    end;
   end;
 end;
 
@@ -130,6 +165,7 @@ begin
   begin
     Panel4.Visible := True;
     Panel5.Visible := True;
+    Panel6.Visible := False;
   end;
 end;
 
@@ -146,15 +182,18 @@ begin
   if PageID = 0 then
     Action_Next.Enabled := (Edit_FullName.Text <> '') and (Edit_UserLogonName.Text <> '')
   else if PageID = 1 then
-    Action_Next.Enabled := True;
+    Action_Next.Enabled := True
 end;
 
 procedure TFrmNewInetOrgPerson.CheckBox_CannotChangeClick(Sender: TObject);
 begin
+  if not CheckBox_CannotChange.Checked then
+    exit;
+  
   if CheckBox_NextLogon.Checked then
   begin
     CheckBox_CannotChange.Checked := False;
-    ShowMessage('You cannot check both User must change password at next logon and User cannot change password for the same user.');
+    ShowMessage('You specified that the password should never expire.' + LineEnding + 'The user will not be required to change the password at next logon.');
   end;
 end;
 
@@ -163,12 +202,15 @@ begin
   if CheckBox_NextLogon.Checked then
   begin
     CheckBox_NextLogon.Checked := False;
-    ShowMessage('You specified that the password should never expire.' + LineEnding + 'The user will not be required to change the password at next logon.');
-  end 
+    ShowMessage('You cannot check both User must change password at next logon and User cannot change password for the same user.');
+  end;
 end;
 
-procedure TFrmNewInetOrgPerson.CheckBox_NextLogonClick(Sender: TObject);
+procedure TFrmNewInetOrgPerson.CheckBox_NextLogonChange(Sender: TObject);
 begin
+  if not CheckBox_NextLogon.Checked then
+     exit;
+  
   if CheckBox_NeverExpires.Checked then
   begin
     CheckBox_NextLogon.Checked := False;
@@ -208,10 +250,6 @@ end;
 procedure TFrmNewInetOrgPerson.Edit_UserLogonNameChange(Sender: TObject);
 begin
   Edit_PreWindowsSuffix.Text := Edit_UserLogonName.Text
-end;
-
-procedure TFrmNewInetOrgPerson.Load;
-begin
 end;
 
 constructor TFrmNewInetOrgPerson.Create(TheOwner: TComponent);
