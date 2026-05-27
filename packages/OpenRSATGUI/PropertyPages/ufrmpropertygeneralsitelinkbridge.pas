ea97fa3de7afcd174747e6cc343bc24787b875bb
mmarsac <mmarsac@tranquil.it>
2026-05-27 12:23:45 +0200

    [NEW] PropertyPage - Add new method to prepare site link lists setup

--- packages/OpenRSATGUI/PropertyPages/ufrmpropertygeneralsitelinkbridge.pas
+++ packages/OpenRSATGUI/PropertyPages/ufrmpropertygeneralsitelinkbridge.pas
@@ -11,17 +11,22 @@ uses
   Controls,
   ExtCtrls,
   StdCtrls,
+  Dialogs,
   mormot.core.base,
   mormot.core.log,
+  mormot.core.text,
+  mormot.net.ldap,
   uhelpersui,
   uproperty,
-  upropertyframe,
+  ursatldapclient,
   ulog;
 
 type
 
-  { TFrmPropertyGeneralSiteLinkBridge }
-
+  { TLdapResultArray }
+  TLdapResultArray = array of TLdapResult;
+  
+  { TFrmPropertyGeneralSiteLinkBridge }  
   TFrmPropertyGeneralSiteLinkBridge = class(TPropertyFrame)
     Button_Add: TButton;
     Button_Remove: TButton;
@@ -41,6 +46,17 @@ type
   private
     fLog: TSynLogClass;
     fProperty: TProperty;
+    fLdap: TRsatLdapClient;
+    
+    fInSite, fNotInSite: TLdapResultArray;
+    
+    procedure AddItemInResultArray(var List: TLdapResultArray; Item: TLdapResult);
+    function SearchSitesInLdap: boolean;
+    function GetSitesInSiteLink: TLdapResultArray;
+    function GetSitesNotInSiteLink: TLdapResultArray;
+    procedure RetrieveSiteLinks;
+    function GetResultName(Obj: TLdapResult): RawUtf8;
+    procedure LoadListBox;
   public
     constructor Create(TheOwner: TComponent); override;
     procedure Update(Props: TProperty); override;
@@ -49,6 +65,70 @@ type
 implementation
 
 {$R *.lfm}
+
+
+procedure TFrmPropertyGeneralSiteLinkBridge.AddItemInResultArray(var List: TLdapResultArray; Item: TLdapResult);
+var
+  ListLen: Integer;
+begin
+  if not Assigned(Item) then
+    exit;
+
+  ListLen := Length(List);
+  SetLength(List, ListLen + 1);
+  List[ListLen] := TLdapResult(Item.Clone);
+end;
+
+
+function TFrmPropertyGeneralSiteLinkBridge.SearchSitesInLdap: boolean;
+begin
+  Result := fLdap.Search(FormatUtf8('CN=Sites,%', [fLdap.ConfigDN]), false, '(&(objectClass=siteLink))', ['name', 'distinguishedName'])
+end;
+
+procedure TFrmPropertyGeneralSiteLinkBridge.RetrieveSiteLinks;
+var
+  LdapResult: TLdapResult;
+begin
+  fLdap.SearchBegin();
+  fLdap.SearchScope := lssWholeSubtree;
+  repeat
+    if not SearchSitesInLdap then
+      Exit;
+
+    for LdapResult in fLdap.SearchResult.Items do
+      AddItemInResultArray(fNotInSite, LdapResult);
+  until fLdap.SearchCookie = '';
+  fLdap.SearchEnd;
+end;
+
+
+function TFrmPropertyGeneralSiteLinkBridge.GetSitesInSiteLink: TLdapResultArray;
+begin
+  Result := fInSite;
+end;
+
+function TFrmPropertyGeneralSiteLinkBridge.GetSitesNotInSiteLink: TLdapResultArray;
+begin
+  Result := fNotInSite;
+end;
+
+function TFrmPropertyGeneralSiteLinkBridge.GetResultName(Obj: TLdapResult): RawUtf8;
+begin
+  Result := Obj.Find('name').GetReadable();
+end;
+
+procedure TFrmPropertyGeneralSiteLinkBridge.LoadListBox;
+var
+  r: TLdapResult;
+begin
+  ListBox_NotInSiteLinkBridge.Clear;
+  for r in GetSitesNotInSiteLink do
+    ListBox_NotInSiteLinkBridge.Items.Add(GetResultName(r));
+
+  ListBox_InSiteLinkBridge.Clear;
+  for r in GetSitesInSiteLink do
+    ListBox_InSiteLinkBridge.Items.Add(GetResultName(r));
+end;
 
 constructor TFrmPropertyGeneralSiteLinkBridge.Create(TheOwner: TComponent);
 begin
@@ -67,9 +147,13 @@ begin
     fLog.Add.Log(sllTrace, 'Update', Self);
 
   fProperty := Props;
+  fLdap := fProperty.LdapClient;
 
   Edit_Name.CaptionNoChange := fProperty.name;
   Edit_Description.CaptionNoChange := fProperty.description;
+
+  RetrieveSiteLinks;
+  LoadListBox;
 end;
 
 end.
