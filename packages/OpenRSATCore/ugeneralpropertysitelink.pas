4caa378f7903f548ad19cae6ea31cdd2d4d6de6c
mmarsac <mmarsac@tranquil.it>
2026-05-29 14:34:28 +0200

    [FIX] PropertyPages - Add missing methods in general properties site link page logic

--- packages/OpenRSATCore/ugeneralpropertysitelink.pas
+++ packages/OpenRSATCore/ugeneralpropertysitelink.pas
@@ -36,12 +36,17 @@ type
     procedure GetAllSites;
     procedure MoveItemToInSite(Index: Integer);
     procedure MoveItemToNotInSite(Index: Integer);
+    procedure SetScalarProperty(const Attribute, Value: RawUtf8; Option: TLdapAddOption);
+    procedure SyncSiteListProperty(Option: TLdapAddOption);
 
     function GetSitesInSiteLink: TLdapResultArray;
     function GetSitesNotInSiteLink: TLdapResultArray;
     function GetResultName(Obj: TLdapResult): RawUtf8;
     function GetSiteAttrValue(List: TLdapResultArray; idx: Integer; attr: RawUtf8): RawUtf8;
     function GetNbSites: Integer;
+    function GetValueFromAttribute(Attribute: TLdapAttribute): RawUtf8; virtual;
+    function FindAttribute(Attribute: RawUtf8): TLdapAttribute; virtual;
+    function FindAttribute(Attribute: RawUtf8; LdapResult: TLdapResult): TLdapAttribute; virtual;
 
     property Ldap: TRsatLdapClient read fLdap;
   end;
@@ -79,7 +84,7 @@ end;
 
 function TGeneralPropertySiteLink.SearchSitesInLdap: boolean;
 begin
-  Result := fLdap.Search(FormatUtf8('CN=Sites,%', [fLdap.ConfigDN]), false, FormatUtf8('(&(objectClass=site))', []), ['name', 'distinguishedName'])
+  Result := fLdap.Search(FormatUtf8('CN=Sites,%', [fLdap.ConfigDN]), false, '(&(objectClass=site))', ['name', 'distinguishedName'])
 end;
 
 
@@ -104,14 +109,43 @@ var
 begin
   fLdap.SearchBegin();
   fLdap.SearchScope := lssSingleLevel;
-  repeat
-    if not SearchSitesInLdap then
-      Exit;
+  try
+     repeat
+     if not SearchSitesInLdap then
+       Exit;
     
-    for LdapResult in fLdap.SearchResult.Items do
-      AddItemInResultArray(fNotInSite, LdapResult);
-  until fLdap.SearchCookie = '';
-  fLdap.SearchEnd;
+     for LdapResult in fLdap.SearchResult.Items do
+       AddItemInResultArray(fNotInSite, LdapResult);
+     until fLdap.SearchCookie = '';
+  finally
+    fLdap.SearchEnd;
+  end;
+end;
+
+procedure TGeneralPropertySiteLink.SetScalarProperty(const Attribute, Value: RawUtf8; Option: TLdapAddOption);
+begin
+  fProperty.Add(Attribute, Value, Option);
+end;
+
+procedure TGeneralPropertySiteLink.SyncSiteListProperty(Option: TLdapAddOption);
+var
+  i: Integer;
+  DN: RawUtf8;
+begin
+  if Length(fInSite) = 0 then
+  begin
+    fProperty.Add('siteList', '', aoReplaceValue);
+    Exit;
+  end;
+
+  DN := fInSite[0].Find('distinguishedName').GetReadable();
+  fProperty.Add('siteList', DN, aoReplaceValue);
+
+  for i := 1 to High(fInSite) do
+  begin
+    DN := fInSite[i].Find('distinguishedName').GetReadable();
+    fProperty.Add('siteList', DN, aoNoDuplicateValue);
+  end;
 end;
 
 function TGeneralPropertySiteLink.GetSitesInSiteLink: TLdapResultArray;
@@ -137,6 +171,24 @@ end;
 function TGeneralPropertySiteLink.GetNbSites: Integer;
 begin
   Result := Length(fInSite) + Length(fNotInSite);
+end;
+
+function TGeneralPropertySiteLink.GetValueFromAttribute(Attribute: TLdapAttribute): RawUtf8;
+begin
+  if Attribute <> nil then
+    Result := Attribute.GetReadable()
+  else
+    Result := '';
+end;
+
+function TGeneralPropertySiteLink.FindAttribute(Attribute: RawUtf8): TLdapAttribute;
+begin
+  Result := fProperty.Attributes.Find(Attribute);
+end;
+
+function TGeneralPropertySiteLink.FindAttribute(Attribute: RawUtf8; LdapResult: TLdapResult): TLdapAttribute;
+begin
+  Result := LdapResult.Attributes.Find(Attribute);
 end;
 
 end.
