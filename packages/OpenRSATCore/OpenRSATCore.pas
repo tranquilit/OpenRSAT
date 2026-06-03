a723e7524167258be3a6e583d8a18c63d707afae
mmarsac <mmarsac@tranquil.it>
2026-06-03 11:18:09 +0200

    [UPD] ModuleADSS - Removed interface file of double list logic and moved it in abstract file

--- packages/OpenRSATCore/OpenRSATCore.pas
+++ packages/OpenRSATCore/OpenRSATCore.pas
@@ -14,8 +14,7 @@ uses
   umoduleadsioption, udns, ugplink, uproperty, usidcache, 
   uadvancedsecuritypresenter, uguidcache, uselectobjectguidpresenter,
   uviewkeytabpresenter, unewsitelink, ugeneralpropertysitelink, ulog, 
-  ugeneralpropertysitelinkbridge, uintfdoublelistlogic, udoublelistlogic, 
-  LazarusPackageIntf;
+  ugeneralpropertysitelinkbridge, udoublelistlogic, LazarusPackageIntf;
 
 implementation
 
