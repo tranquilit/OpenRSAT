153d6b832d939d4012c26022bda303f43bcc6a76
mmarsac <mmarsac@tranquil.it>
2026-06-03 11:18:09 +0200

    [UPD] ModuleADSS - Removed interface file of double list logic and moved it in abstract file

--- packages/OpenRSATCore/udoublelistlogic.pas
+++ packages/OpenRSATCore/udoublelistlogic.pas
@@ -11,12 +11,30 @@ uses
   mormot.core.text,
   mormot.net.ldap,
   uproperty,
-  uintfdoublelistlogic,
   ursatldapclient;
 
 type  
+  { TLdapResultArray }
   TLdapResultArray = array of TLdapResult;
 
+  { ISiteLinkLogic }
+  IDoubleListLogic = interface
+    procedure GetAllResources;
+    procedure MoveItemToInSite(Index: Integer);
+    procedure MoveItemToNotInSite(Index: Integer);
+    procedure SetScalarProperty(const Attribute, Value: RawUtf8; Option: TLdapAddOption);
+    procedure SyncAttributeProperty(Option: TLdapAddOption);
+    procedure AddToNotInSite(Item: TLdapResult);
+    procedure AddToInSite(Item: TLdapResult);
+    function GetItemAttrValue(List: TLdapResultArray; idx: Integer; attr: RawUtf8): RawUtf8;
+    function GetResultName(Obj: TLdapResult): RawUtf8;
+    function GetNbSites: Integer;
+    function GetValueFromAttribute(Attribute: TLdapAttribute): RawUtf8;
+    function FindAttribute(Attribute: RawUtf8): TLdapAttribute;
+    function FindAttribute(Attribute: RawUtf8; LdapResult: TLdapResult): TLdapAttribute;
+  end;
+  
+  { TDoubleListLogic }
   TDoubleListLogic = class abstract(TInterfacedObject, IDoubleListLogic)
   private
     fProperty: TProperty;
