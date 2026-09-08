ca8e3c951a63f46b565b66c814a8590c1e708580
tprudhomme <tprudhomme@tranquil.it>
2026-09-08 12:18:35 +0200

    [NEW] ADUC - Update values in PSO property frame

--- packages/OpenRSATGUI/PropertyPages/ufrmpropertypso.pas
+++ packages/OpenRSATGUI/PropertyPages/ufrmpropertypso.pas
@@ -13,6 +13,10 @@ uses
   ExtCtrls,
   Buttons,
   tis.ui.grid.core,
+  mormot.core.base,
+  mormot.core.text,
+  mormot.core.variants,
+  mormot.net.ldap,
   uproperty,
   upropertyframe;
 
@@ -23,42 +27,42 @@ type
   TFrmPropertyPSO = class(TPropertyFrame)
     BitBtn_Add: TBitBtn;
     BitBtn_Remove: TBitBtn;
-    CheckBox3: TCheckBox;
-    CheckBox4: TCheckBox;
-    Edit1: TEdit;
-    Edit2: TEdit;
-    Edit3: TEdit;
-    Edit4: TEdit;
-    Edit5: TEdit;
-    Edit6: TEdit;
-    Edit7: TEdit;
-    Edit8: TEdit;
+    CheckBox_PwdComplexity: TCheckBox;
+    CheckBox_PwdReversibleEncryption: TCheckBox;
+    Edit_Name: TEdit;
+    Edit_Precedence: TEdit;
+    Edit_MinPwdLength: TEdit;
+    Edit_PwdHistoryLength: TEdit;
+    Edit_MinPwdAge: TEdit;
+    Edit_MaxPwdAge: TEdit;
+    Edit_LockoutPwdObservationWindow: TEdit;
+    Edit_LockoutPwdThreshold: TEdit;
     GroupBox_PasswordSettings: TGroupBox;
     GroupBox_AppliesTo: TGroupBox;
-    Label1: TLabel;
-    Label10: TLabel;
-    Label11: TLabel;
-    Label2: TLabel;
-    Label3: TLabel;
-    Label4: TLabel;
-    Label5: TLabel;
-    Label6: TLabel;
-    Label7: TLabel;
-    Label8: TLabel;
-    Panel1: TPanel;
-    Panel10: TPanel;
-    Panel11: TPanel;
-    Panel2: TPanel;
-    Panel3: TPanel;
-    Panel4: TPanel;
-    Panel5: TPanel;
-    Panel6: TPanel;
-    Panel7: TPanel;
-    Panel8: TPanel;
+    Label_Name: TLabel;
+    Label_LockoutPwdThreshold: TLabel;
+    Label_LockoutPwdObservationWindow: TLabel;
+    Label_Precedence: TLabel;
+    Label_MinPwdLength: TLabel;
+    Label_PwdHistoryLength: TLabel;
+    Label_PwdComplexity: TLabel;
+    Label_PwdReversibleEncryption: TLabel;
+    Label_MinPwdAge: TLabel;
+    Label_MaxPwdAge: TLabel;
+    Panel_Name: TPanel;
+    Panel_LockoutPwdThreshold: TPanel;
+    Panel_LockoutPwdObservationWindow: TPanel;
+    Panel_Precedence: TPanel;
+    Panel_MinPwdLength: TPanel;
+    Panel_PwdHistoryLength: TPanel;
+    Panel_PwdComplexity: TPanel;
+    Panel_PwdReversibleEncryption: TPanel;
+    Panel_MinPwdAge: TPanel;
+    Panel_MaxPwdAge: TPanel;
     Panel9: TPanel;
     TisGrid_AppliesTo: TTisGrid;
   private
-
+    fProperty: TProperty;
   public
     constructor Create(TheOwner: TComponent); override;
     procedure Update(Props: TProperty); override;
@@ -79,8 +83,65 @@ begin
 end;
 
 procedure TFrmPropertyPSO.Update(Props: TProperty);
+var
+  AppliesTo: TLdapAttribute;
+  i: Integer;
+  AppliesToData, SearchResultData, Row: TDocVariantData;
+  Filter: RawUtf8;
+  DN: RawByteString;
+  P: PDocVariantData;
 begin
+  fProperty := Props;
 
+  Edit_Name.Text := fProperty.name;
+  Edit_Precedence.Text := fProperty.GetRaw('msDS-PasswordSettingsPrecedence');
+  Edit_MinPwdLength.Text := fProperty.GetRaw('msDS-MinimumPasswordLength');
+  Edit_PwdHistoryLength.Text := fProperty.GetRaw('msDS-PasswordHistoryLength');
+  CheckBox_PwdComplexity.Checked := fProperty.GetRaw('msDS-PasswordComplexityEnabled') <> 'FALSE';
+  CheckBox_PwdReversibleEncryption.Checked := fProperty.GetRaw('msDS-PasswordReversibleEncryptionEnabled') <> 'FALSE';
+  Edit_MinPwdAge.Text := fProperty.GetRaw('msDS-MinimumPasswordAge');
+  Edit_MaxPwdAge.Text := fProperty.GetRaw('msDS-MaximumPasswordAge');
+  Edit_LockoutPwdThreshold.Text := fProperty.GetRaw('msDS-LockoutThreshold');
+  Edit_LockoutPwdObservationWindow.Text := fProperty.GetRaw('msDS-LockoutObservationWindow');
+
+  TisGrid_AppliesTo.Clear;
+  AppliesTo := fProperty.Get('appliesTo');
+  if Assigned(AppliesTo) then
+  begin
+    Filter := '';
+    for i := 0 to AppliesTo.Count - 1 do
+       Filter := FormatUtf8('%(distinguishedName=%)', [Filter, LdapEscape(AppliesTo.GetRaw())]);
+    if Filter = '' then
+      Exit;
+    Filter := FormatUtf8('(|%)', [Filter]);
+
+    if not fProperty.LdapClient.SearchAllDocRaw(SearchResultData, fProperty.LdapClient.DefaultDN, Filter, ['name', 'mail'], [roRawValues, roObjectNameAtRoot, roKnownValuesAsArray]) then
+      Exit;
+
+    AppliesToData.Init(JSON_FAST);
+    TisGrid_AppliesTo.BeginUpdate;
+    try
+      for i := 0 to AppliesTo.Count - 1 do
+      begin
+        DN := AppliesTo.GetRaw(i);
+        if not SearchResultData.Exists(DN) then
+          Continue;
+        P := SearchResultData.O[DN];
+        if not Assigned(P) then
+          Continue;
+
+        Row.Init(JSON_FAST);
+        Row.U['distinguishedName'] := AppliesTo.GetRaw(i);
+        Row.U['name'] := P^.U['name'];
+        Row.U['mail'] := P^.U['mail'];
+        AppliesToData.AddItem(Row);
+        Row.Clear;
+      end;
+    finally
+      TisGrid_AppliesTo.EndUpdate;
+      TisGrid_AppliesTo.LoadData(@AppliesToData);
+    end;
+  end;
 end;
 
 end.
